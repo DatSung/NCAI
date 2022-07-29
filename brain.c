d0bd7cec618262c2be8026418d85a9e4ae149339
DatSung-Dev <datsung.dev@gmail.com>
2022-07-29 20:52:05 +0700

    update

--- brain.c
+++ brain.c
@@ -3,9 +3,3 @@
 #include <ctype.h>
 #include <time.h>
 
-int main(){
-    char hardLevel;
-    printf("---WELCOME T0 NUMBER COUNT AI---\n");
-    printf("--PLEASE CHOOSE THE HARD LEVEL--\n");
-    scanf("%f", hardLevel);
-}
