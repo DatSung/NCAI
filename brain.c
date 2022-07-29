6b01c4d88f71ab57816e44cd7a44429f5f04609a
DatSung-Dev <datsung.dev@gmail.com>
2022-07-29 20:41:36 +0700

    update

--- brain.c
+++ brain.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
 int main(){
-    printf("\nWellcome to NumbersCount AI\n\n");
-    return 0;
+    char hardLevel;
+    printf("---WELCOME T0 NUMBER COUNT AI---\n");
+    printf("--PLEASE CHOOSE THE HARD LEVEL--\n");
+    scanf("%f", hardLevel);
 }
