be48cff5682a2eb7ccd7861bdee1fbc675fcd516
DatSung-Dev <datsung.dev@gmail.com>
2022-09-12 03:07:04 +0700

    update

--- brain.c
+++ brain.c
@@ -3,5 +3,101 @@
 #include <ctype.h>
 #include <time.h>
 
-int main(){
+int main(void){
+    int main_Num = 0, player_Num = 0, ai_Num = 0;
+    int key_control = 0;
+
+    printf("Welcome to NCAI Early Access\n");
+    printf("Please choose difficult level\n");
+    printf("(1) Stupid AI\n");
+    printf("(2) Smart AI\n");
+    scanf("%d", &key_control);
+
+    if (key_control == 1) {
+
+        do {
+
+            if (main_Num < 30) {
+                printf("\n");
+
+                printf("PLease enter your number count: ");
+
+                do {
+                    scanf("%d", &player_Num);
+                } while (player_Num > 3);
+
+                printf("Main number: %d\n", main_Num += player_Num);
+
+                // CHECK RESULT
+
+                if (main_Num >= 30)
+                {
+                    printf("YOU WIN");
+                }
+                
+
+            }
+
+            if (main_Num < 30) {
+
+                printf("AI count: %d\n", ai_Num = rand() % 3);
+                printf("Main number: %d\n", main_Num += ai_Num);
+
+                printf("\n");
+
+                // CHECK RESULT
+                if (main_Num >= 30)
+                {
+                    printf("YOU WIN");
+                }
+
+        }
+
+    } while (main_Num < 30);
+
+
+
+    } else if (key_control == 2) {
+
+        do {
+
+        printf("\n");
+
+        printf("PLease enter your number count: ");
+        do {
+            scanf("%d", &player_Num);
+        } while (player_Num > 3);
+        printf("Main number: %d\n", main_Num += player_Num);
+
+        if (player_Num == 1) {
+            ai_Num = 3;
+            printf("AI count: %d\n", ai_Num);
+        } else if (player_Num == 3) {
+            ai_Num = 1;
+            printf("AI count: %d\n", ai_Num);
+        } else {
+            ai_Num = 2;
+            printf("AI count: %d\n", ai_Num);
+        }
+
+
+        printf("Main number: %d\n", main_Num += ai_Num);
+
+        printf("\n");
+
+        if (main_Num + player_Num >= 30)
+        {
+            printf("YOU WIN\n");
+        } else if (main_Num + ai_Num >= 30)
+        {
+            printf("AI WIN");
+        }
+
+    } while (main_Num < 30);
+
+    };
+    
+    
+
+    
 }
