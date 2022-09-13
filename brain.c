35f15e3c11c27cbb8d216b64c00830f8087576d8
DatSung-Dev <datsung.dev@gmail.com>
2022-09-13 14:31:26 +0700

    update

--- brain.c
+++ brain.c
@@ -13,6 +13,8 @@ int main(void){
     printf("Please choose difficult level\n");
     printf("(1) Stupid AI\n");
     printf("(2) Smart AI\n");
+    printf("(3) Smart Player\n");
+    printf("(4) Stupid Player\n");
     scanf("%d", &key_control);
 
     if (key_control == 1) {
@@ -100,6 +102,9 @@ int main(void){
                 if (main_Num == 1) {
                     ai_Num = 1;
                 };
+                if (main_Num == 3) {
+                    ai_Num = 1;
+                }
 
                 printf("AI count: %d\n", ai_Num);
                 printf("Main number: %d\n", main_Num += ai_Num);
@@ -111,10 +116,64 @@ int main(void){
                 {
                     printf("AI WIN\n");
                 }
+            }
 
-        }
+        } while (main_Num < 30);
 
-    } while (main_Num < 30);
-    };
+    } else if (key_control == 3) {
+
+        do {
+            if (main_Num < 30) {
+                
+                printf("\n");
+                printf("AI count: %d\n", ai_Num = rand() % 3);
+                printf("Main number: %d\n", main_Num += ai_Num);
+
+                printf("\n");
+
+                // CHECK RESULT
+                if (main_Num >= 30)
+                {
+                    printf("AI WIN\n");
+                }
+
+            }
+
+            if (main_Num < 30) {
+                printf("\n");
+
+                printf("PLease enter your number count: ");
+
+                do {
+                    scanf("%d", &player_Num);
+                } while (player_Num > 3);
+
+                printf("Main number: %d\n", main_Num += player_Num);
+
+                // CHECK RESULT
+
+                if (main_Num >= 30)
+                {
+                    printf("YOU WIN");
+                }
+                
+
+            }
+
+        } while (main_Num < 30);
+
+    } else if (key_control == 4) {
+        do
+        {
+            
+            if (main_Num < 30)
+            {
+                
+            }
+            
+
+        } while (main_Num <30);
+        
+    }
     
 }
