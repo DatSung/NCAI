5fdd645679eb14deed5286b7f781ad83777472d5
DatSung-Dev <datsung.dev@gmail.com>
2022-09-15 17:19:34 +0700

    update

--- brain.c
+++ brain.c
@@ -3,6 +3,8 @@
 #include <ctype.h>
 #include <time.h>
 
+
+
 int main(void){
     int main_Num = 0, player_Num = 0, ai_Num = 0;
     int key_control = 0;
@@ -11,10 +13,10 @@ int main(void){
 
     printf("Welcome to NCAI Early Access\n");
     printf("Please choose difficult level\n");
-    printf("(1) Stupid AI\n");
-    printf("(2) Smart AI\n");
-    printf("(3) Smart Player\n");
-    printf("(4) Stupid Player\n");
+    printf("(1) Stupid AI (Player go first)\n");
+    printf("(2) Smart AI (Player go first)\n");
+    printf("(3) Smart Player (AI go first)\n");
+    printf("(4) Stupid Player (AI go first)\n");
     scanf("%d", &key_control);
 
     if (key_control == 1) {
@@ -28,6 +30,10 @@ int main(void){
 
                 do {
                     scanf("%d", &player_Num);
+                    if (player_Num > 3)
+                    {
+                        printf("Please enter less than 3!\n");
+                    }
                 } while (player_Num > 3);
 
                 printf("Main number: %d\n", main_Num += player_Num);
@@ -70,6 +76,10 @@ int main(void){
 
                 do {
                     scanf("%d", &player_Num);
+                    if (player_Num > 3)
+                    {
+                        printf("Please enter less than 3!\n");
+                    }
                 } while (player_Num > 3);
 
                 printf("Main number: %d\n", main_Num += player_Num);
@@ -146,6 +156,10 @@ int main(void){
 
                 do {
                     scanf("%d", &player_Num);
+                    if (player_Num > 3)
+                    {
+                        printf("Please enter less than 3!\n");
+                    }
                 } while (player_Num > 3);
 
                 printf("Main number: %d\n", main_Num += player_Num);
@@ -166,12 +180,71 @@ int main(void){
         do
         {
             
-            if (main_Num < 30)
-            {
+            if (main_Num < 30) {   
+
+                // AI ENGINE
+                if (player_Num == 1) {
+                    ai_Num = 3;
+                } else if (player_Num == 2)
+                {                   
+                    ai_Num = 2;   
+                } else if (player_Num == 3)
+                {
+                    ai_Num = 1;
+                };
+
+                if (main_Num == 1) {
+                    ai_Num = 1;
+                };
+                if (main_Num == 3) {
+                    ai_Num = 1;
+                }
+                
+                if (main_Num == 0)
+                {
+                    ai_Num = 2;
+                }
+                // // //
+
+                printf("\n");
+                printf("AI count: %d\n", ai_Num);
+                printf("Main number: %d\n", main_Num += ai_Num);
+
+                printf("\n");
+
+                // CHECK RESULT
+                if (main_Num >= 30)
+                {
+                    printf("AI WIN\n");
+                }
+            }
+
+            if (main_Num < 30) {
+
+                printf("\n");
+
+                printf("PLease enter your number count: ");
+
+                do {
+                    scanf("%d", &player_Num);
+                    if (player_Num > 3)
+                    {
+                        printf("Please enter less than 3!\n");
+                    }
+                    
+                } while (player_Num > 3);
+
+                printf("Main number: %d\n", main_Num += player_Num);
+
+                // CHECK RESULT
+
+                if (main_Num >= 30)
+                {
+                    printf("YOU WIN");
+                }
                 
             }
             
-
         } while (main_Num <30);
         
     }
